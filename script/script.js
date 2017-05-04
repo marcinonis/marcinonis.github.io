43d48bb5a552b1e8104356b1b4103b47fd1b02e6
Marciokas <martynas@marcinonis.lt>
2017-05-05 01:23:36 +0300

    home button

--- script/script.js
+++ script/script.js
@@ -78,3 +78,26 @@ $('a').click(function() {
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 700);
 });
+
+//-----------------
+
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
+});
